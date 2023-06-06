2387093f4280223af9e63c63d58a17ef8f833218
Sebastian Guerrero <wesdell17@gmail.com>
2023-06-06 09:13:36 -0500

    custom hook useCategory for fetching

---
+++ hooks/useCategories.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+import { type ICategory } from '@/interfaces';
+import { getAllCategories } from '@/services';
+
+export const useCategories = () => {
+  const [categories, setCategories] = useState<ICategory[]>();
+
+  useEffect(() => {
+    getAllCategories()
+      .then(setCategories);
+  }, []);
+  
+  return {
+    categories
+  };
+};
