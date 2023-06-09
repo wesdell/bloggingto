0deeb2c1750be3e8ef49cf1d57c058e934464dc9
Wesdell <wesdell17@gmail.com>
2023-06-09 10:31:09 -0500

    Adding new interfaces for comment section

---
+++ interfaces/comment.ts
@@ -0,0 +1,12 @@
+export interface Comment {
+  name: string
+  email: string
+  slug: string
+  comment: string
+}
+
+export interface MinifiedComment {
+  name: string
+  createdAt: string
+  comment: string
+}
